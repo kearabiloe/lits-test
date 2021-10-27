3181ae90aec9ec416d6b6d4ef41fe36f3d9fe603
kearabiloe <kea@crowdcoin.top>
2021-10-27 17:32:38 +0200

    [Added] User model and pip requirements

--- core/models.py
+++ core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    '''
+    A model to store user information
+    '''
+    avatar = models.ImageField(null=True, blank=True, upload_to='users_avatars')
