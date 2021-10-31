9a4c8d3ae3131ef8749888bb62f98c8f2765f381
kearabiloe <kearabiloe.ledwaba@gmail.com>
2021-10-31 17:22:30 +0200

    [Feature] Exposes Primary API Endpoints

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import *
@@ -17,3 +17,45 @@ class VendorView(LoginRequiredMixin,View):
         context['orders'] = orders
         context['vendors'] = vendors
         return render(request, self.template_name, context)
+
+
+class OrderView(LoginRequiredMixin,View):
+    '''
+    View for creating an order
+    '''
+    template_name = "./order.html"
+
+    def get(self, request, *args, **kwargs):
+        context ={}
+        materials = Material.objects.filter(vendor=request.user.vendor)
+        vendors = Vendor.objects.all()
+        context['materials'] = materials
+        context['vendors'] = vendors
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        context ={}
+        print(request.POST)
+        materials = []
+        for item in request.POST:
+            #Build Order materials
+            print(item)
+            if item.startswith('material_'):
+                materials.append( (item.split('_')[1],request.POST.get(item)))
+        vendor = Vendor.objects.get(id=request.POST.get('vendor'))
+        order = Order.objects.create(
+            vendor=vendor,
+            placed_by=request.user,
+            delivery_date=request.POST.get('delivery_date'),
+            comment=request.POST.get('comment')
+        )
+        for item in materials:
+            print(item)
+            material = Material.objects.get(id=item[0])
+            order_item = OrderItem.objects.create(
+                material=material,
+                quantity=item[1],
+                )
+            order.items.add(order_item)
+        order.save()
+        return redirect('vendor')
